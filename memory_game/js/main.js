4dd0d934d28ee2dfb8a2f8bdbd7e12c783d83c94
MalcolmLow83 <malcomlow@gmail.com>
2019-06-16 15:15:02 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,56 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+},
+{
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+},
+{
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+},
+{
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+}
+];
+
+
 var cardsInPlay = [];
 
 function flipCard(cardId) {
 
 //Display flipped card function
-console.log("User flipped " + cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
 
 //store cards value into cardsInPlay array
-cardsInPlay.push(cardId);
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 
+//Check if 2 cards matches
+if (cardsInPlay.length < 2) {
+    console.log("Flip your second card")
+
+} else if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!")
+    console.log(cardsInPlay[0])
+
+} else {
+    alert("Sorry, try again")
+}
+};
+
+flipCard(0);
+flipCard(2);
+
+/*
 //Check if 2 cards matches
 if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
     console.log("You found a match!")
@@ -17,19 +59,6 @@ if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
     console.log("Sorry, try again")
 
 } else {
-    console.log("Only played 1 card")
-}
-};
-
-flipCard(0);
-flipCard(2);
-
-
-/*
-//Check if 2 cards have been played
-if (cardsInPlay.length === 2) {
-    console.log("User have played 2 cards");
-} else {
-    console.log("User have not played 2 cards");
+    console.log("Flip your second card")
 }
 */
