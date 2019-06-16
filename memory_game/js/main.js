d3db42bcec6bed45457421e21646fa5f429bd01f
MalcolmLow83 <malcomlow@gmail.com>
2019-06-16 10:02:37 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,44 +1,35 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne;
-var cardTwo;
-var msg = "";
 
-//store cards[0] value into cardOne
-cardOne = cards[0];
+function flipCard(cardId) {
 
-//store cards[2] value into cardTwo
-cardTwo = cards[2];
+//Display flipped card function
+console.log("User flipped " + cards[cardId]);
 
-//store cardOne value into cardsInPlay[0]
-cardsInPlay.push(cardOne);
-
-//store cardOne value into cardsInPlay[1]
-cardsInPlay.push(cardTwo);
-
-//Display cardsInPlay
-alert("User flipped " + cardsInPlay[0] + " and " + cardsInPlay[1]);
-
-//Check if 2 cards have been played
-if (cardsInPlay.length === 2) {
-    alert("User have played 2 cards");
-} else {
-    alert("User have not played 2 cards");
-}
+//store cards value into cardsInPlay array
+cardsInPlay.push(cardId);
 
 //Check if 2 cards matches
-if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!")
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!")
+
+} else if (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1]) {
+    console.log("Sorry, try again")
+
 } else {
-    alert("Sorry, try again")
+    console.log("Only played 1 card")
 }
+};
+
+flipCard(0);
+flipCard(2);
+
 
 /*
-console.log("Up and running");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+//Check if 2 cards have been played
+if (cardsInPlay.length === 2) {
+    console.log("User have played 2 cards");
+} else {
+    console.log("User have not played 2 cards");
+}
 */
